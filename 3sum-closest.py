3c8ce32bfde70702fe2c9b7a82730f907c29bc76
sasa <>
2018-09-17 22:24:09 +0800

    3sum-closest与3sum的区别在于，后者将三个数的和与0做大小对比，判断遍历方式，前者将三个数的和减target数的差值与最小差值做大小对比，更新最小差值，判读遍历方式。

---
+++ 3sum-closest.py
@@ -0,0 +1,40 @@
+'''
+Given an array nums of n integers and an integer target,
+find three integers in nums such that the sum is closest to target.
+Return the sum of the three integers.
+You may assume that each input would have exactly one solution.
+
+Example:
+
+Given array nums = [-1, 2, 1, -4], and target = 1.
+
+The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+'''
+class Solution:
+    def threeSumClosest(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        nums, i, min_diff, sum = sorted(nums), 0, float("inf"), float("inf")
+        while i < len(nums) - 2:
+            if i == 0 or nums[i] != nums[i - 1]:
+                j, k = i + 1, len(nums) - 1
+                diff = nums[i] + nums[j] + nums[k] - target
+                if abs(diff) < min_diff:
+                    min_diff = abs(diff)
+                    sum = nums[i] + nums[j] + nums[k]
+                if diff < 0:
+                    j += 1
+                elif diff > 0:
+                    k -= 1
+                else:
+                    return target
+            i += 1
+        return sum
+
+so = Solution()
+print(so.threeSumClosest([-1, 2, 1, -4], 1))
+
+
