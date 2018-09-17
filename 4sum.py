5600faa98fee7adf9c30d49d5f580bf6b00ffa45
sasa <>
2018-09-17 23:50:56 +0800

    4sum解法参考了3sum思路，在外层多加了一层循环。

---
+++ 4sum.py
@@ -0,0 +1,55 @@
+'''
+Given an array nums of n integers and an integer target,
+are there elements a, b, c, and d in nums such that a + b + c + d = target?
+Find all unique quadruplets in the array which gives the sum of target.
+
+Note:
+
+The solution set must not contain duplicate quadruplets.
+
+Example:
+
+Given array nums = [1, 0, -1, 0, -2, 2], and target = 0.
+
+A solution set is:
+[
+  [-1,  0, 0, 1],
+  [-2, -1, 1, 2],
+  [-2,  0, 0, 2]
+]
+'''
+import collections
+class Solution1:
+    def fourSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        result = []
+        for i in range(0, len(nums) - 3): # 注意边界
+            if i and nums[i] == nums[i - 1]: # 等价于 i == 0 or nums[i] != nums[i - 1]
+                continue
+            for j in range(i + 1, len(nums) - 2):
+                if j != i + 1 and nums[j] == nums[j - 1]:
+                    continue
+                sum = target - nums[i] - nums[j]
+                left, right = j + 1, len(nums) - 1
+                while left < right:
+                    if nums[left] + nums[right] == sum:
+                        result.append([nums[i], nums[j], nums[left], nums[right]])
+                        left += 1
+                        right -= 1
+                        while left < right and nums[left] == nums[left - 1]:
+                            left += 1
+                        while left < right and nums[right] == nums[right + 1]:
+                            right -= 1
+                    elif nums[left] + nums[right] < sum:
+                        left += 1
+                    else:
+                        right -= 1
+        return  result
+
+so = Solution1()
+print(so.fourSum([1, 0, -1, 0, -2, 2], 0))
